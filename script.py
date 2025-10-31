1ab2bf54bf251f2849282c18a38e38617b0afd6b
JohnMThompson <johnmichaelthomp@gmail.com>
2025-10-31 17:12:46 -0500

    updated script, added requirements.txt

--- script.py
+++ script.py
@@ -9,16 +9,17 @@ source = "Wings"
 ct = datetime.datetime.now()
 
 # Fetch mortgage rates
-URL = "https://www.wingsfinancial.com/rates/mortgages-purchase"
+URL = "https://www.wingscu.com/rates/mortgage-loans"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 table = soup.find_all('td')
 
-fr30 = float(str(table[0])[4:9])
-fr30pt = float(str(table[2])[4:6])
-ARM71 = float(str(table[25])[4:9])
-ARM71pt = float(str(table[7])[4:6])
+fr30 = float(str(table[0]).replace(" ","").replace("\t","").replace("\n","")[4:10])
+fr30pt = float(str(table[2]).replace(" ","").replace("\t","").replace("\n","")[4:6])
+
+ARM71 = float(str(table[25]).replace(" ","").replace("\t","").replace("\n","")[4:9])
+ARM71pt = float(str(table[7]).replace(" ","").replace("\t","").replace("\n","")[4:6])
 
 # Connect to database
 db = mysql.connector.connect(
@@ -30,7 +31,7 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 # Write to database
-sql = "INSERT INTO `daily_rates` (source, timestamp, 30_year_fixed_rate, 30_year_fixed_points, 71_arm_rate,71_arm_point) VALUES (%s,%s,%s,%s,%s,%s)"
+sql = "INSERT INTO `daily_rates` (`source`, `timestamp`, `30_year_fixed_rate`, `30_year_fixed_points`, `71_arm_rate`,`71_arm_point`) VALUES (%s,%s,%s,%s,%s,%s)"
 val = (source,ct,fr30,fr30pt,ARM71,ARM71pt)
 cursor.execute(sql,val)
 
